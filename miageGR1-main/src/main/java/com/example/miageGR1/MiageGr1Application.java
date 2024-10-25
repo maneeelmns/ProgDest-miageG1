1c3fd887f71fd0a016ad225f6f8e42c63ab730c4
mansouri <manel.mansouri@etu.u-paris.fr>
2024-10-25 14:15:01 +0200

    newcarservice

--- miageGR1-main/src/main/java/com/example/miageGR1/MiageGr1Application.java
+++ miageGR1-main/src/main/java/com/example/miageGR1/MiageGr1Application.java
@@ -3,6 +3,7 @@ package com.example.miageGR1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// main
 @SpringBootApplication
 public class MiageGr1Application {
 

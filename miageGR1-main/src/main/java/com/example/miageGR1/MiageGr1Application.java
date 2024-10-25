cbd79f2c44ede2c8ae0338cbd1ee0f451f355197
mansouri <manel.mansouri@etu.u-paris.fr>
2024-10-25 14:10:39 +0200

    initial commit

---
+++ miageGR1-main/src/main/java/com/example/miageGR1/MiageGr1Application.java
@@ -0,0 +1,13 @@
+package com.example.miageGR1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageGr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiageGr1Application.class, args);
+	}
+
+}
